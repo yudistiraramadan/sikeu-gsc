2b897711bd6a262a0a3adf1803c01083589ba784
yudistira-rk <yudistiraeducation100@gmail.com>
2022-12-06 16:39:59 +0700

    06/12/2022 before styling

--- resources/views/dashboard/relawan.blade.php
+++ resources/views/dashboard/relawan.blade.php
@@ -17,15 +17,13 @@
     <div class="row">
         <div class="col-6 col-lg-3 col-md-6">
             <div class="card">
-                <div class="card-body px-4 py-4-5">
+                <div class="card-body">
                     <div class="row">
                         <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
-                            <div class="stats-icon green mb-2">
-                                <i class="iconly-boldProfile"></i>
-                            </div>
+                                <i class="bi bi-people" style="color:#5ddab4; font-size:36pt;"></i>
                         </div>
                         <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                            <h6 class="text-muted font-semibold"><b>Total Relawan GSC</b></h6>
+                            <h6 style="font-weight: 500;" class="text-muted font-semibold"><b>Total Relawan GSC</b></h6>
                             <h4 class="font-extrabold mb-0">{{ $total_data }} orang</h4>
                         </div>
                     </div>
@@ -34,6 +32,54 @@
         </div>
 
         <div class="col-6 col-lg-3 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                                <i class="bi bi-person" style="color:#26a0fc; font-size:36pt;"></i>
+                        </div>
+                        <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                            <h6 style="font-weight: 500;" class="text-muted font-semibold"><b>Total Relawan Laki-laki</b></h6>
+                            <h4 class="font-extrabold mb-0">{{ $pria }} orang</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-6 col-lg-3 col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                                <i class="bi bi-person" style="color:#FF8DC7; font-size:36pt;"></i>
+                        </div>
+                        <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                            <h6 style="font-weight: 500;" class="text-muted font-semibold"><b>Total Relawan Perempuan</b></h6>
+                            <h4 class="font-extrabold mb-0">{{ $wanita }} orang</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-6 col-lg-3 col-md-6">
+            <div class="card">
+                <div class="card-body px-4 py-4-5">
+                    <div class="d-flex align-items-center">
+                        <div class="avatar avatar-xl">
+                            <img src="foto-relawan/gsc.png" alt="Face 1">
+                        </div>
+                        <div class="ms-3 name">
+                            <h6 class="font-bold">{{ $data->name }}</h6>
+                            <p class="text-muted mb-0">{{ $data->email }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- <div class="col-6 col-lg-3 col-md-6">
             <div class="card">
                 <div class="card-body px-4 py-4-5">
                     <div class="row">
@@ -68,23 +114,9 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
 
-        <div class="col-6 col-lg-3 col-md-6">
-            <div class="card">
-                <div class="card-body px-4 py-4-5">
-                    <div class="d-flex align-items-center">
-                        <div class="avatar avatar-xl">
-                            <img src="foto-relawan/yudis.JPG" alt="Face 1">
-                        </div>
-                        <div class="ms-3 name">
-                            <h5 class="font-bold">{{ $data->name }}</h5>
-                            <h6 class="text-muted mb-0">{{ $data->email }}</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
     </div>
 
     {{-- Statistik Relawan --}}
@@ -110,6 +142,8 @@
                 </div>
             </div>
         </div>
+
+        
     </div>
 @endsection
 @push('scripts')
